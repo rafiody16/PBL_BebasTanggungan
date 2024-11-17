dc89442691462dd0e1033fbb2aee082db0cbcb8e
Tama Dioo <tamadio91@gmail.com>
2024-11-17 18:49:14 +0700

    Menambahkan template Mazer

--- Koneksi.php
+++ Koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 
-$serverName = "DESKTOP-L75SO4G"; //Nama server dapat dilihat di ssms.
+$serverName = "LAPTOP-6DB90827"; //Nama server dapat dilihat di ssms.
 $connectionInfo = array( "Database"=>"PBLBebasTanggungan");  
   
 /* Connect using Windows Authentication. */  
@@ -9,6 +9,8 @@ if( $conn === false )
 {  
      echo "Unable to connect.</br>";  
      die( print_r( sqlsrv_errors(), true));  
-}  
+}  else {
+     echo "Koneksi berhasil";
+}
 
 ?>
