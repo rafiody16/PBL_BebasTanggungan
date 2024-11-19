60fd6f4135a2892f63dc836d216a6c118ec4ed4e
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-11-19 07:53:38 +0700

    fix table user

--- Koneksi.php
+++ Koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 
-$serverName = "LAPTOP-6DB90827"; //Nama server dapat dilihat di ssms.
+$serverName = "DESKTOP-L75SO4G"; //Nama server dapat dilihat di ssms.
 $connectionInfo = array( "Database"=>"PBLBebasTanggungan");  
   
 /* Connect using Windows Authentication. */  
@@ -9,8 +9,5 @@ if( $conn === false )
 {  
      echo "Unable to connect.</br>";  
      die( print_r( sqlsrv_errors(), true));  
-}  else {
-     echo "Koneksi berhasil";
-}
-
+} 
 ?>
