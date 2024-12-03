e190a9b7d9d8971e0edb9f6b11de670ca908d277
Tama Dioo <tamadio91@gmail.com>
2024-12-03 21:08:18 +0700

    try to fix

--- Login/ProsesLogin.php
+++ Login/ProsesLogin.php
@@ -18,54 +18,43 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
         // Verifikasi password (gunakan password_hash dan password_verify untuk keamanan)
-        if (password_verify($Password, $row['Password']) && $row['Role_ID'] == 9) {
+        if (password_verify($Password, $row['Password'])) {
             // Login berhasil
             $_SESSION['Username'] = $Username;
             $_SESSION['Role_ID'] = $row['Role_ID'];
             $_SESSION['ID_User'] = $row['ID_User'];
-
-            // Ambil NIM berdasarkan ID_User
-            $sqlNim = "SELECT NIM FROM Mahasiswa WHERE ID_User = ?";
-            $paramsNim = array($row['ID_User']);
-            $stmtNim = sqlsrv_query($conn, $sqlNim, $paramsNim);
-
-            if ($stmtNim === false) {
-                die(print_r(sqlsrv_errors(), true));
+    
+            // Ambil NIM jika role adalah mahasiswa
+            if ($row['Role_ID'] == '9') {
+                $sqlNim = "SELECT NIM FROM Mahasiswa WHERE ID_User = ?";
+                $paramsNim = array($row['ID_User']);
+                $stmtNim = sqlsrv_query($conn, $sqlNim, $paramsNim);
+    
+                if ($stmtNim === false) {
+                    die(print_r(sqlsrv_errors(), true));
+                }
+    
+                if ($rowNim = sqlsrv_fetch_array($stmtNim, SQLSRV_FETCH_ASSOC)) {
+                    $_SESSION['NIM'] = $rowNim['NIM'];
+                }
+    
+                sqlsrv_free_stmt($stmtNim);
+                echo "<script>alert('Selamat Datang ".$Username.".'); window.location.href = '../User/mahasiswa/dashboardUser.html'; </script>";
             }
-
-            if ($rowNim = sqlsrv_fetch_array($stmtNim, SQLSRV_FETCH_ASSOC)) {
-                $_SESSION['NIM'] = $rowNim['NIM'];
+            // Tambahkan logika untuk role admin
+            else if ($row['Role_ID'] == '1') {
+                echo "<script>alert('Selamat Datang Admin ".$Username.".'); window.location.href = '../Admin/dashboardAdmin.html'; </script>";
             }
-
-            sqlsrv_free_stmt($stmtNim);
-
-            echo "<script>alert('Selamat Datang ".$Username.".'); window.location.href = '../User/mahasiswa/dashboardUser.html'; </script>";
-        } else if ($row['Role_ID'] == 1) {
-            echo "<script>alert('Selamat Datang Admin ".$Username.".'); window.location.href = '../index.php'; </script>";
-        } else if ($row['Role_ID'] == 2) {
-            echo "<script>alert('Selamat Datang Dosen ".$Username.".'); window.location.href = '../Dosen/dashboardDosen.html'; </script>";
-        } else if ($row['Role_ID'] == 3) {
-            echo "<script>alert('Selamat Datang Admin ".$Username.".'); window.location.href = '../index.php'; </script>";
-        } else if ($row['Role_ID'] == 4) {
-            echo "<script>alert('Selamat Datang Admin ".$Username.".'); window.location.href = '../index.php'; </script>";
-        } else if ($row['Role_ID'] == 5) {
-            echo "<script>alert('Selamat Datang Admin ".$Username.".'); window.location.href = '../index.php'; </script>";
-        } else if ($row['Role_ID'] == 6) {
-            echo "<script>alert('Selamat Datang Admin ".$Username.".'); window.location.href = '../index.php'; </script>";
-        } else if ($row['Role_ID'] == 7) {
-            echo "<script>alert('Selamat Datang Admin ".$Username.".'); window.location.href = '../index.php'; </script>";
-        } else if ($row['Role_ID'] == 8) {
-            echo "<script>alert('Selamat Datang Admin ".$Username.".'); window.location.href = '../index.php'; </script>";
-        } else if ($row['Role_ID'] == 9) {
-            echo "<script>alert('Selamat Datang Admin ".$Username.".'); window.location.href = '../index.php'; </script>";
-        } else {
-            echo "<script>alert('Username atau password salah.'); window.location.href = 'Login.php'; </script>";
-        }
+            // Tambahkan logika untuk role lainnya
+            else if ($row['Role_ID'] == '2') {
+                echo "<script>alert('Selamat Datang Dosen ".$Username.".'); window.location.href = '../Dosen/dashboardDosen.html'; </script>";
+            }
     } else {
         echo "<script>alert('Username atau password salah.'); window.location.href = 'Login.php'; </script>";
     }
 
     sqlsrv_free_stmt($stmt);
+    }
 }
 
 sqlsrv_close($conn);
