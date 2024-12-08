ff5d300f71946969a69c8306ee45b5dd344d6f1a
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-12-08 16:22:25 +0700

    GeneratePDF

---
+++ Berkas/GeneratePdf.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+require('../assets/library/fpdf.php');
+include('ProsesBerkas.php');
+
+Berkas();
+$NIM = $_SESSION['NIM'];
+
+// Buat instance FPDF
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial', '', 12);
+
+// Tambahkan header
+$pdf->Image('../assets/img/logopoltek.png', 10, 6, 20);
+$pdf->Image('../assets/img/logojti.png', 180, 9, 15);
+$pdf->Cell(80); // Geser ke tengah
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(30, 10, 'KEMENTERIAN PENDIDIKAN, KEBUDAYAAN, RISET, DAN TEKNOLOGI', 0, 1, 'C');
+$pdf->SetFont('Arial', '', 10);
+$pdf->Cell(190, 5, 'JURUSAN TEKNOLOGI INFORMASI - POLITEKNIK NEGERI MALANG', 0, 1, 'C');
+$pdf->Ln(10);
+
+// Tambahkan data utama
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(0, 10, 'BEBAS TANGGUNGAN JURUSAN', 0, 1, 'C');
+$pdf->Ln(5);
+
+// Tambahkan informasi pribadi
+$pdf->SetFont('Arial', '', 10);
+$pdf->Cell(40, 7, 'Nama', 1);
+$pdf->Cell(0, 7, ' '.$namaMHS, 1, 1);
+$pdf->Cell(40, 7, 'NIM', 1);
+$pdf->Cell(0, 7, ' '.$NIM, 1, 1);
+$pdf->Cell(40, 7, 'Program Studi', 1);
+$pdf->Cell(0, 7, ' '.$Prodi, 1, 1);
+$pdf->Ln(5);
+
+// Tambahkan tabel Tugas Akhir
+$pdf->SetFont('Arial', 'B', 10);
+$pdf->Cell(0, 10, 'Tugas Akhir', 0, 1);
+$pdf->SetFont('Arial', '', 10);
+$pdf->Cell(80, 7, 'Laporan TA', 1);
+$pdf->Cell(20, 7, '     V', 1); // Checklist
+$pdf->Ln();
+$pdf->Cell(80, 7, 'File Aplikasi', 1);
+$pdf->Cell(20, 7, '     V', 1); // Checklist
+$pdf->Ln();
+$pdf->Cell(80, 7, 'Pernyataan Publikasi', 1);
+$pdf->Cell(20, 7, '     V', 1); // Checklist
+$pdf->Ln(10);
+
+// Tambahkan verifikasi Tugas Akhir
+$pdf->Cell(40, 7, 'Tanggal Verifikasi:', 0);
+$pdf->Cell(50, 7, ' '.$tglStrTA, 0, 1);
+$pdf->Cell(40, 7, 'Verifikator:', 0);
+$pdf->Cell(50, 7, ' '.$vrfTA, 0, 1);
+$pdf->Ln(10);
+
+// Tambahkan tabel Administrasi
+$pdf->SetFont('Arial', 'B', 10);
+$pdf->Cell(0, 10, 'Administrasi', 0, 1);
+$pdf->SetFont('Arial', '', 10);
+$pdf->Cell(80, 7, 'Laporan Skripsi', 1);
+$pdf->Cell(20, 7, '     V', 1); // Checklist
+$pdf->Ln();
+$pdf->Cell(80, 7, 'Bebas Kompensasi', 1);
+$pdf->Cell(20, 7, '     V', 1); // Checklist
+$pdf->Ln();
+$pdf->Cell(80, 7, 'Laporan Magang', 1);
+$pdf->Cell(20, 7, '     V', 1); // Checklist
+$pdf->Ln();
+$pdf->Cell(80, 7, 'Scan Toeic', 1);
+$pdf->Cell(20, 7, '     V', 1); // Checklist
+$pdf->Ln(10);
+
+// Tambahkan verifikasi Administrasi
+$pdf->Cell(40, 7, 'Tanggal Verifikasi:', 0);
+$pdf->Cell(50, 7, ' '.$tglStrAdm, 0, 1);
+$pdf->Cell(40, 7, 'Verifikator:', 0);
+$pdf->Cell(50, 7, ' '.$vrfAdm, 0, 1);
+
+// Tambahkan footer
+
+$pdf->Ln(10);
+$pdf->SetFont('Arial', '', 10);
+
+// Bagian Kiri
+$pdf->Cell(95, 10, 'Malang, '.$tglStrBrks, 0, 0, 'L');
+$pdf->Cell(95, 10, 'Malang, '.$tglStrBrks, 0, 0, 'R'); // Tanggal di kiri
+$pdf->Cell(95, 10, '', 0, 1); // Kosong untuk mengimbangi kanan
+$pdf->Cell(95, 10, 'Ketua Jurusan Teknologi Informasi', 0, 0, 'L'); // Posisi kiri
+$pdf->Cell(95, 10, 'Koordinator Program Studi', 0, 1, 'R'); // Posisi kanan
+
+$pdf->Ln(20); // Jarak untuk tanda tangan
+
+// Nama dan Gelar
+$pdf->Cell(95, 10, $vrfKajur, 0, 0, 'L'); // Nama kiri
+$pdf->Cell(95, 10, $vrfKaprodi, 0, 1, 'R'); // Nama kanan
+
+// Output PDF
+$pdf->Output('I', 'bebas_tanggungan_jurusan.pdf');
+?>
--- Berkas/ProsesBerkas.php
+++ Berkas/ProsesBerkas.php
@@ -616,6 +616,64 @@ function EditAdministrasi() {
     error_reporting(E_ALL);
 }
 
+function Berkas() {
+    global $conn;
+
+    global $namaMHS, $Prodi, $tglVrfAdm, $vrfAdm, $tglVrfTA, $vrfTA, $tglStrAdm, $tglStrTA, $tglStrBrks,
+           $vrfKajur, $vrfKaprodi;
+
+    $NIM = $_GET['NIM'];
+
+    $sql = "SELECT m.NIM, m.Nama, m.Prodi FROM Administrasi AS a INNER JOIN Pengumpulan AS p ON a.ID_Pengumpulan = p.ID_Pengumpulan INNER JOIN 
+            Mahasiswa AS m ON p.NIM = m.NIM WHERE m.NIM = ?";
+    $params = array($NIM);
+    $stmt = sqlsrv_query($conn, $sql, $params);
+
+    if ($stmt === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    if ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        $namaMHS = $row['Nama'];
+        $Prodi = $row['Prodi'];
+    } 
+
+    $sql2 = "SELECT a.Tanggal_Verifikasi, a.Verifikator FROM Administrasi AS a INNER JOIN Pengumpulan AS p ON a.ID_Pengumpulan = p.ID_Pengumpulan INNER JOIN 
+            Mahasiswa AS m ON p.NIM = m.NIM WHERE m.NIM = ?";
+    $params2 = array($NIM);
+    $stmt2 = sqlsrv_query($conn, $sql2, $params2);
+
+    if ($row2 = sqlsrv_fetch_array($stmt2, SQLSRV_FETCH_ASSOC)) {
+        $tglVrfAdm = $row2['Tanggal_Verifikasi'];
+        $tglStrAdm = $tglVrfAdm->format('d-m-Y');
+        $vrfAdm = $row2['Verifikator'];
+    }
+
+    $sql3 = "SELECT a.Tanggal_Verifikasi, a.Verifikator FROM TugasAkhir AS a INNER JOIN Pengumpulan AS p ON a.ID_Pengumpulan = p.ID_Pengumpulan INNER JOIN 
+            Mahasiswa AS m ON p.NIM = m.NIM WHERE m.NIM = ?";
+    $params3 = array($NIM);
+    $stmt3 = sqlsrv_query($conn, $sql3, $params3);
+
+    if ($row3 = sqlsrv_fetch_array($stmt3, SQLSRV_FETCH_ASSOC)) {
+        $tglVrfTA = $row3['Tanggal_Verifikasi'];
+        $tglStrTA = $tglVrfTA->format('d-m-Y');
+        $vrfTA = $row3['Verifikator'];
+    }
+
+    $sql4 = "SELECT p.Tanggal_Verifikasi, p.VerifikatorKajur, p.VerifikatorKaprodi FROM Pengumpulan AS p INNER JOIN 
+            Mahasiswa AS m ON p.NIM = m.NIM WHERE m.NIM = ?";
+    $params4 = array($NIM);
+    $stmt4 = sqlsrv_query($conn, $sql4, $params4);
+
+    if ($row4 = sqlsrv_fetch_array($stmt4, SQLSRV_FETCH_ASSOC)) {
+        $tglVrfBrks = $row4['Tanggal_Verifikasi'];
+        $tglStrBrks = $tglVrfBrks->format('d-m-Y');
+        $vrfKajur = $row4['VerifikatorKajur'];
+        $vrfKaprodi = $row4['VerifikatorKaprodi'];
+    }
+
+}
+
 
 $sql = "SELECT a.ID_Administrasi, m.NIM, m.Nama, a.Status_Verifikasi, a.Keterangan FROM Administrasi AS a
         INNER JOIN Pengumpulan AS p ON a.ID_Pengumpulan = p.ID_Pengumpulan INNER JOIN Mahasiswa AS m ON p.NIM = m.NIM";
