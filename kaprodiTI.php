5261ff953f8a086c2ea26f32797f9b5f1cafb6ed
rezfahreza <143343397+rezfahreza@users.noreply.github.com>
2024-12-09 15:39:49 +0700

    edit dasborad kaprodi

--- kaprodiTI.php
+++ kaprodiTI.php
@@ -215,12 +215,25 @@ if ($_SESSION['Role_ID'] != 3) {
         </header>
 
         <div class="page-heading">
-          <h3>Profile Statistics</h3>
+          <h3>Selamat Datang di BeTaTI!</h3>
         </div>
         <div class="page-content">
           <section class="row">
-            <div class="col-12 col-lg-9">
-              <div class="row">
+          <div class="col-20 col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                  <div class="" style="padding: 20px; border-radius: 100px;">
+                    <h5>Halo, Ketua Program Studi TI!</h5>
+                    <p>Selamat datang di beranda sistem informasi Bebas Tanggungan Jurusan Teknologi Informasi (BeTaTI). 
+                        Ini adalah tempat di mana Anda dapat mengakses semua informasi penting yang berkaitan dengan bebas tanggungan mahasiswa jurusan Teknologi Informasi dan berbagai layanan akademik lainnya.</p>
+                  </div>
+                </div>
+              </div>
+          </div>
+          </section>
+        </div>
+        <div class="page-content">
+          <section class="row">
                 <div class="col-6 col-lg-3 col-md-6">
                   <div class="card">
                     <div class="card-body px-4 py-4-5">
@@ -299,278 +312,6 @@ if ($_SESSION['Role_ID'] != 3) {
                     </div>
                   </div>
                 </div>
-              </div>
-              <div class="row">
-                <div class="col-12">
-                  <div class="card">
-                    <div class="card-header">
-                      <h4>Profile Visit</h4>
-                    </div>
-                    <div class="card-body">
-                      <div id="chart-profile-visit"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-12 col-xl-4">
-                  <div class="card">
-                    <div class="card-header">
-                      <h4>Profile Visit</h4>
-                    </div>
-                    <div class="card-body">
-                      <div class="row">
-                        <div class="col-7">
-                          <div class="d-flex align-items-center">
-                            <svg
-                              class="bi text-primary"
-                              width="32"
-                              height="32"
-                              fill="blue"
-                              style="width: 10px"
-                            >
-                              <use
-                                xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill"
-                              />
-                            </svg>
-                            <h5 class="mb-0 ms-3">Europe</h5>
-                          </div>
-                        </div>
-                        <div class="col-5">
-                          <h5 class="mb-0 text-end">862</h5>
-                        </div>
-                        <div class="col-12">
-                          <div id="chart-europe"></div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-7">
-                          <div class="d-flex align-items-center">
-                            <svg
-                              class="bi text-success"
-                              width="32"
-                              height="32"
-                              fill="blue"
-                              style="width: 10px"
-                            >
-                              <use
-                                xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill"
-                              />
-                            </svg>
-                            <h5 class="mb-0 ms-3">America</h5>
-                          </div>
-                        </div>
-                        <div class="col-5">
-                          <h5 class="mb-0 text-end">375</h5>
-                        </div>
-                        <div class="col-12">
-                          <div id="chart-america"></div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-7">
-                          <div class="d-flex align-items-center">
-                            <svg
-                              class="bi text-success"
-                              width="32"
-                              height="32"
-                              fill="blue"
-                              style="width: 10px"
-                            >
-                              <use
-                                xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill"
-                              />
-                            </svg>
-                            <h5 class="mb-0 ms-3">India</h5>
-                          </div>
-                        </div>
-                        <div class="col-5">
-                          <h5 class="mb-0 text-end">625</h5>
-                        </div>
-                        <div class="col-12">
-                          <div id="chart-india"></div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-7">
-                          <div class="d-flex align-items-center">
-                            <svg
-                              class="bi text-danger"
-                              width="32"
-                              height="32"
-                              fill="blue"
-                              style="width: 10px"
-                            >
-                              <use
-                                xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill"
-                              />
-                            </svg>
-                            <h5 class="mb-0 ms-3">Indonesia</h5>
-                          </div>
-                        </div>
-                        <div class="col-5">
-                          <h5 class="mb-0 text-end">1025</h5>
-                        </div>
-                        <div class="col-12">
-                          <div id="chart-indonesia"></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-12 col-xl-8">
-                  <div class="card">
-                    <div class="card-header">
-                      <h4>Latest Comments</h4>
-                    </div>
-                    <div class="card-body">
-                      <div class="table-responsive">
-                        <table class="table table-hover table-lg">
-                          <thead>
-                            <tr>
-                              <th>Name</th>
-                              <th>Comment</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td class="col-3">
-                                <div class="d-flex align-items-center">
-                                  <div class="avatar avatar-md">
-                                    <img src="../assets/compiled/jpg/5.jpg" />
-                                  </div>
-                                  <p class="font-bold ms-3 mb-0">Si Cantik</p>
-                                </div>
-                              </td>
-                              <td class="col-auto">
-                                <p class="mb-0">
-                                  Congratulations on your graduation!
-                                </p>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td class="col-3">
-                                <div class="d-flex align-items-center">
-                                  <div class="avatar avatar-md">
-                                    <img src="assets/compiled/jpg/2.jpg" />
-                                  </div>
-                                  <p class="font-bold ms-3 mb-0">Si Ganteng</p>
-                                </div>
-                              </td>
-                              <td class="col-auto">
-                                <p class="mb-0">
-                                  Wow amazing design! Can you make another
-                                  tutorial for this design?
-                                </p>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td class="col-3">
-                                <div class="d-flex align-items-center">
-                                  <div class="avatar avatar-md">
-                                    <img src="assets/compiled/jpg/8.jpg" />
-                                  </div>
-                                  <p class="font-bold ms-3 mb-0">
-                                    Singh Eknoor
-                                  </p>
-                                </div>
-                              </td>
-                              <td class="col-auto">
-                                <p class="mb-0">
-                                  What a stunning design! You are so talented
-                                  and creative!
-                                </p>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td class="col-3">
-                                <div class="d-flex align-items-center">
-                                  <div class="avatar avatar-md">
-                                    <img src="assets/compiled/jpg/3.jpg" />
-                                  </div>
-                                  <p class="font-bold ms-3 mb-0">Rani Jhadav</p>
-                                </div>
-                              </td>
-                              <td class="col-auto">
-                                <p class="mb-0">
-                                  I love your design! Its so beautiful and
-                                  unique! How did you learn to do this?
-                                </p>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-12 col-lg-3">
-              <div class="card">
-                <div class="card-body py-4 px-4">
-                  <div class="d-flex align-items-center">
-                    <div class="avatar avatar-xl">
-                      <img src="assets/compiled/jpg/1.jpg" alt="Face 1" />
-                    </div>
-                    <div class="ms-3 name">
-                      <h5 class="font-bold">Dio Andika P. M. T.</h5>
-                      <h6 class="text-muted mb-0">2341720098</h6>
-                      <!-- <h6 class="text-muted mb-0">D-IV Teknik Informatika</h6> -->
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card">
-                <div class="card-header">
-                  <h4>Recent Messages</h4>
-                </div>
-                <div class="card-content pb-4">
-                  <div class="recent-message d-flex px-4 py-3">
-                    <div class="avatar avatar-lg">
-                      <img src="assets/compiled/jpg/4.jpg" />
-                    </div>
-                    <div class="name ms-4">
-                      <h5 class="mb-1">Hank Schrader</h5>
-                      <h6 class="text-muted mb-0">@johnducky</h6>
-                    </div>
-                  </div>
-                  <div class="recent-message d-flex px-4 py-3">
-                    <div class="avatar avatar-lg">
-                      <img src="assets/compiled/jpg/5.jpg" />
-                    </div>
-                    <div class="name ms-4">
-                      <h5 class="mb-1">Dean Winchester</h5>
-                      <h6 class="text-muted mb-0">@imdean</h6>
-                    </div>
-                  </div>
-                  <div class="recent-message d-flex px-4 py-3">
-                    <div class="avatar avatar-lg">
-                      <img src="assets/compiled/jpg/1.jpg" />
-                    </div>
-                    <div class="name ms-4">
-                      <h5 class="mb-1">John Dodol</h5>
-                      <h6 class="text-muted mb-0">@dodoljohn</h6>
-                    </div>
-                  </div>
-                  <div class="px-4">
-                    <button
-                      class="btn btn-block btn-xl btn-outline-primary font-bold mt-3"
-                    >
-                      Start Conversation
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div class="card">
-                <div class="card-header">
-                  <h4>Visitors Profile</h4>
-                </div>
-                <div class="card-body">
-                  <div id="chart-visitors-profile"></div>
-                </div>
-              </div>
-            </div>
           </section>
         </div>
 
