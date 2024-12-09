be3dcb8f770a6a84c7cf90508a2123f53929bcc0
rezfahreza <143343397+rezfahreza@users.noreply.github.com>
2024-12-09 16:15:11 +0700

    edit tambah staff

--- User/FormStaff.php
+++ User/FormStaff.php
@@ -170,8 +170,8 @@ getDataStaffByNip();
                 <div class="page-title">
                     <div class="row">
                         <div class="col-12 col-md-6 order-md-1 order-last">
-                            <h3>Tambah Data Mahasiswa</h3>
-                            <p class="text-subtitle text-muted">Tambahkan data mahasiswa</p>
+                            <h3>Tambah Data Staff</h3>
+                            <p class="text-subtitle text-muted">Tambahkan data staff</p>
                         </div>
                     <div class="col-12 col-md-6 order-md-2 order-first">
                     <nav
